284ee14789460947ce2ed37f3b8f728d29c4d425
kyawthetpaing1997 <thetpaing9984kyaw@gmail.com>
2026-09-22 10:51:50 +0630

    Refactor bot.py with token placeholder and comments
    Updated bot token placeholder and cleaned up comments.

--- bot.py
+++ bot.py
@@ -1,34 +1,26 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
-# BotFather ဆီက ရထားတဲ့ သင့်ရဲ့ Token ကို ဒီနေရာမှာ ထည့်ပါ
-TOKEN = '8686636473:AAGUGWzpHJ1hKfzQ1h8azB86poonWAG9jNI'
-
-
+TOKEN = 'သင့်ရဲ့_ဘော့တိုကင်_ထည့်ပါ'
 bot = telebot.TeleBot(TOKEN)
 
 bot.remove_webhook()
-# Web App လင့်ခ်နဲ့ ပုံလင့်ခ်ကို ဒီနေရာမှာ ထည့်ပါ။ 
+
 WEB_APP_URL = 'https://wondrous-crostata-02d61c.netlify.app/'
 IMAGE_URL = 'https://imgur.com/a/IMrdKeo'
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    # ဖန်တီးမယ့် Web App ခလုတ်
     markup = InlineKeyboardMarkup()
     web_app = WebAppInfo(url=WEB_APP_URL)
-    ​markup.add(InlineKeyboardButton("🚀 Start Mine MRCT 🚀", web_app=web_app))
+    markup.add(InlineKeyboardButton("🚀 Start Mine MRCT 🚀", web_app=web_app))
     
-
-
+    welcome_text = "👋 Hello! Welcome to MRCT Official Bot 👋\n\nYou Can Start Mining MRCT Journeys.\n👇👇👇👇👇👇👇👇👇"
     
-  # ကြိုဆိုစာနှင့် ပုံကို ပပို့ပေးခြင်း welcome_text = "👋 Hello! Welcome to MRCT Official Bot 👋\n\nYou Can Start Mining MRCT Journeys.\n👇👇👇👇👇👇👇👇👇"
-
-👇👇👇👇👇👇👇👇👇
     bot.send_photo(
-        message.chat.id, 
-        IMAGE_URL, 
-        caption=welcome_text, 
+        message.chat.id,
+        IMAGE_URL,
+        caption=welcome_text,
         reply_markup=markup
     )
 
